b7e15887e8277aa50e335f0ba38073c876fb007e
Juan Pablo Agudelo <juapaagu@bancolombia.com.co>
2023-10-16 19:46:34 -0500

    Se agregan operaciones matematicas a la funcion lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,25 @@
 import json
+import util as utl
 import pandas as pd
 
 def lambda_handler(event, context):
+    
+    operation = event['operation']
+    num1 = event['num1']
+    num2 = event['num2']
+    print("RESULTADO DE LA OPERACION")
+    if(operation == 'SUMA'):
+        print(utl.suma(num1, num2))
+    elif(operation == 'RESTA'):
+        print(utl.resta(num1, num2))
+    if(operation == 'MULTIPLICACION'):
+        print(utl.multiplicacion(num1, num2))
+
     dummyData = {
         'col1':[1,2], 
         'col2':[3,4]
     }
     df = pd.DataFrame(data = dummyData)
     print(df)
-    print('Done x2.0')
+    print('Done x3.0')
     print('DESPLIEGUE DE FUNCION LAMBDA EN PYTHON USANDO AWS CODEBUILD')
---
+++ util.py
@@ -0,0 +1,8 @@
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
